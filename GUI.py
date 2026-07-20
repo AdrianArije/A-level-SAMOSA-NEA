738f24e37a51cc03c725b9056c2d092fa8053929
Adrian Arije <adrianarije@gmail.com>
2026-07-20 21:17:29 +0100

    Login GUI look Version 1

--- GUI.py
+++ GUI.py
@@ -3,7 +3,8 @@ class MainApp: # initializing the MainApp class which serves as the controller
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("SAMOSA")
-        self.root.geometry("800x800")
+        self.root.geometry("800x600")
+        
 
         self.frames = { # this dictionary would contain all GUI classes
             "login" : LoginFrame(self)
@@ -24,13 +25,31 @@ class LoginFrame(tk.Frame): # login frame initialization
     def __init__(self,app):
         super().__init__(app.root) # inherits the main root window as the place to be displayed
         self.app = app
-        tk.Label(self, # label saying login
-                 text="Login Page",
-                  font=("Arial",20)).pack(pady=20)
+        self.configure(background="#3a79ee")
 
-        tk.Button(self, # button to login
-                  text="Login",
-                  command=self.login).pack()
+        intro = tk.Label(self,text = "Welcome to Adrian's Canteen ",font= ("Arial", 43),bg="#3a79ee", foreground="#fbf025")
+        intro.grid(row=0,column = 1,columnspan=2, padx = 10, pady =15)
+
+        self.entry_frame = tk.Frame(self)
+        self.entry_frame.place(x=300,y=220)
+
+        lgntext = tk.Label(self.entry_frame,text = "Login In / Sign up",font= ("Arial", 20))
+        lgntext.grid(row=0,column = 0, columnspan=2)
+
+        username_label = tk.Label(self.entry_frame,text = "Username",font = ("Arial",10))
+        username_label.grid(row=1,column = 0)
+
+        username_entry =tk.Entry(self.entry_frame)
+        username_entry.grid(row=1,column = 1, padx = 12, pady =10)
+
+        password_label = tk.Label(self.entry_frame,text = "Password",font = ("Arial",10))
+        password_label.grid(row=2,column = 0)
+
+        password_entry =tk.Entry(self.entry_frame)
+        password_entry.grid(row=2,column = 1, padx = 12, pady =10)
+
+        loginbtn = tk.Button(self.entry_frame, text="Login", command=self.login)
+        loginbtn.grid(row=3,column = 0,columnspan = 2, padx = 12, pady =10)
         
     def login(self):
         # Normally you'd check the database here.
