01452cabc0e8080caa6791f96f8c8f47497ab3d5
Adrian Arije <adrianarije@gmail.com>
2026-07-22 20:38:01 +0100

    added the Cart page as well as the close button

--- GUI.py
+++ GUI.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------
 class MainApp: # initializing the MainApp class which serves as the controller
     def __init__(self):
         self.root = tk.Tk()
@@ -21,37 +23,39 @@ class MainApp: # initializing the MainApp class which serves as the controller
             current_frame.pack_forget()
         frame.pack(fill = "both", expand=True)
 
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------
 class LoginFrame(tk.Frame): # login frame initialization
     def __init__(self,app):
         super().__init__(app.root) # inherits the main root window as the place to be displayed
         self.app = app
         self.configure(background="#3a79ee")
 
-        intro = tk.Label(self,text = "Welcome to Adrian's Canteen ",font= ("Arial", 43),bg="#3a79ee", foreground="#fbf025")
+        intro = tk.Label(self,text = "Welcome to Adrian's Canteen ",font= ("Arial", 43),bg="#3a79ee", foreground="#fbf025") # intro text label
         intro.grid(row=0,column = 1,columnspan=2, padx = 10, pady =15)
 
-        self.entry_frame = tk.Frame(self)
+        self.entry_frame = tk.Frame(self) # creating new frame for entry widgets
         self.entry_frame.place(x=280,y=220)
 
-        lgntext = tk.Label(self.entry_frame,text = "    Login In / Sign up    ",font= ("Arial", 20))
+        lgntext = tk.Label(self.entry_frame,text = "    Login In / Sign up    ",font= ("Arial", 20)) # login label
         lgntext.grid(row=0,column = 0, columnspan=2)
 
-        username_label = tk.Label(self.entry_frame,text = "Username",font = ("Arial",10))
+        username_label = tk.Label(self.entry_frame,text = "Username",font = ("Arial",10)) # username label
         username_label.grid(row=1,column = 0)
 
-        username_entry =tk.Entry(self.entry_frame,width=20)
+        username_entry =tk.Entry(self.entry_frame,width=20) # username entry box
         username_entry.grid(row=1,column = 1, padx = 12, pady =10)
 
-        password_label = tk.Label(self.entry_frame,text = "Password",font = ("Arial",10))
+        password_label = tk.Label(self.entry_frame,text = "Password",font = ("Arial",10)) # password entry label
         password_label.grid(row=2,column = 0)
 
-        password_entry =tk.Entry(self.entry_frame,width=20)
+        password_entry =tk.Entry(self.entry_frame,width=20) # password entry box
         password_entry.grid(row=2,column = 1, padx = 12, pady =10)
 
-        loginbtn = tk.Button(self.entry_frame, text="Login", command=self.login)
+        loginbtn = tk.Button(self.entry_frame, text="Login", command=self.login) # login button
         loginbtn.grid(row=3,column = 0, pady=10)
 
-        signupbtn = tk.Button(self.entry_frame, text="Sign Up", command=self.signup)
+        signupbtn = tk.Button(self.entry_frame, text="Sign Up", command=self.signup) # sign up button
         signupbtn.grid(row=3,column = 1, pady=5)
 
     def login(self):
@@ -63,42 +67,66 @@ class LoginFrame(tk.Frame): # login frame initialization
          self.app.show_frame(self.app.frames["customerframe"])
     print("Login Frame Initialized")
 
+
+#----------------------------------------------------------------------------------------------------------------------------------
 class CustomerFrame(tk.Frame): # customer frame initialization
     def __init__(self,app):
         super().__init__(app.root) # inherits the main root window as the place to be displayed
         self.app = app
         self.configure(background="red")
 
-        header_frame = tk.Frame(self)
+        self.cartframe= CartFrame(self) # linking cart frame as a sub class of customer frame
+        self.cartframe.place_forget() # hiding the cart frame
+
+        header_frame = tk.Frame(self,bg ="red") # crating header frame for header
         header_frame.pack()
 
-        content_frame = tk.Frame(self, bg="red")
+        content_frame = tk.Frame(self, bg="red") # creating content frame for items
         content_frame.pack(fill="both", expand=True)
 
-        intro = tk.Label(header_frame,text = "Menu ",font= ("Arial", 43),bg="red", foreground="#fbf025",anchor="center")
-        intro.pack(pady=10)
+        intro = tk.Label(header_frame,text = "Menu Items",font= ("Arial", 43),bg="red", foreground="#fbf025",anchor="center") # intro text
+        intro.grid(row=0,column=1,columnspan=2)
 
-        search_entry = tk.Entry(content_frame, width=90)
-        search_entry.grid(row=0, column=0, padx = 40)
+        search_entry = tk.Entry(content_frame, width=90) # search entry box
+        search_entry.grid(row=0, column=0, padx = 25)
 
-        searchbtn = tk.Button(content_frame, text="Search", command=self.search)
-        searchbtn.grid(row=0, column=1,)
+        searchbtn = tk.Button(content_frame, text="Search", command=self.search) # search button
+        searchbtn.grid(row=0, column=1,padx = 5)
 
-        filter_btn = tk.Menubutton(content_frame,text = "Filter ▼", relief="raised")
-        filter_btn.grid(row=0,column=2)
+        filter_btn = tk.Menubutton(content_frame,text = "Filter ▼", relief="raised") # adding the filter button menu
+        filter_btn.grid(row=0,column=2,padx= 5) # set postion
         dropdown = tk.Menu(filter_btn,tearoff=0)
-        self.drinks_var = tk.BooleanVar()
-        dropdown.add_checkbutton(label = "Drinks",variable = self.drinks_var)
-        dropdown.add_command(label = "By Price",command = self.filterprice)
-        dropdown.add_separator()
-        dropdown.add_command(label = "By Name",command = self.filtername)
-        filter_btn["menu"] = dropdown
+        self.drinks_var = tk.BooleanVar() # making a variable to check if box is ticked
+        dropdown.add_checkbutton(label = "Drinks",variable = self.drinks_var) # adding button checkbox
+        filter_btn["menu"] = dropdown # linking button and dropdown
+
+        sort_btn = tk.Menubutton(content_frame,text = "Sort By ▼", relief="raised") # adding the sort button menu
+        sort_btn.grid(row=0,column=3, padx=5) # set position
+        sort_dropdown = tk.Menu(sort_btn,tearoff=0)
+        sort_dropdown.add_command(label = "By Price",command = self.filterprice) # adding sorting button
+        sort_dropdown.add_separator()
+        sort_dropdown.add_command(label = "By Name",command = self.filtername)
+        sort_btn["menu"] = sort_dropdown # link button to drop down
 # for the filter use a label frame to hide options and user has to press search
 # for sorting use a ttk.combobox
 
-        logoutbtn = tk.Button(content_frame, text="Logout", command=self.logout)
-        logoutbtn.grid(row=1,column = 1, padx = 12, pady =10)
+        options_btn = tk.Menubutton(header_frame,text = "▼ OPTIONS", relief="raised",bg="red") # creating a menu drop down for options
+        options_btn.grid(row=0,column=8,padx= 10)
+        options_dropdown= tk.Menu(options_btn,tearoff=0)
+        options_dropdown.add_command(label="CART",command=self.cart) # adding all the options with commands
+        options_dropdown.add_command(label="ORDER HISTORY",command=self.orderpage)
+        options_dropdown.add_command(label="SETTINGS",command=self.settings)
+        options_dropdown.add_command(label="LOGOUT",command=self.logout)
+        options_btn["menu"] = options_dropdown # likning the button with the menu
 
+    def settings(self):
+        pass
+    def orderpage(self):
+        pass
+    def cart(self):
+        self.cartframe.place(relx=0.5, rely=0.5, anchor="center")
+        self.cartframe.lift()
+        print("cart")
     def logout(self):
         # Normally you'd check the database here.
         self.app.show_frame(self.app.frames["login"])  
@@ -112,5 +140,18 @@ class CustomerFrame(tk.Frame): # customer frame initialization
     def filtername(self):
         pass
     print("Customer Frame Initialized")
+
+class CartFrame(tk.Frame): # customer frame initialization
+    def __init__(self,parent):
+        super().__init__(parent) # inherits the main root window as the place to be displayed
+        self.configure(background="brown",width=400,height=400)
+        self.pack_propagate(False)
+        close_btn = tk.Button(self,text = "Close", command=self.close)
+        close_btn.place(x=180,y=370)
+        
+
+
+    def close(self):
+        self.place_forget()
 MainApp()
 
