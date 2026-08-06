f0b5484f7d3c32baa1110fec3068193ffebaf8c3
Adrian Arije <adrianarije@gmail.com>
2026-08-06 16:46:13 +0100

    Cart has been created next checkout

--- GUI.py
+++ GUI.py
@@ -105,7 +105,7 @@ class CustomerFrame(tk.Frame): # Customer frame initialization
         self.cartframe= CartFrame(self) # Linking cart frame as a sub class of customer frame
         self.cartframe.place_forget() # Hiding the cart frame
 
-        header_frame = tk.Frame(self,bg ="red") # crating header frame for header
+        header_frame = tk.Frame(self,bg ="red") # creating header frame for header
         header_frame.pack()
 
         content_frame = tk.Frame(self, bg="yellow") # Creating content frame for items
@@ -114,19 +114,8 @@ class CustomerFrame(tk.Frame): # Customer frame initialization
         toolbar_frame = tk.Frame(content_frame, bg="yellow")
         toolbar_frame.pack(fill="x")
 
-        canvas_frame = tk.Frame(content_frame, bg="yellow")
-        canvas_frame.pack(fill="both", expand=True)
-
-        scrollbar = tk.Scrollbar(canvas_frame,orient="vertical")
-        scrollbar.pack(side="right",fill="y")
-
-        self.canvas = tk.Canvas(canvas_frame,yscrollcommand=scrollbar.set)
-        scrollbar.config(command=self.canvas.yview)
-        self.item_frame = tk.Frame(self.canvas,bg = "brown")
-        self.canvas.create_window((0,0),window=self.item_frame,anchor="nw")
-        self.canvas_window = self.canvas.create_window((0,0),window=self.item_frame,anchor="nw")
-        self.item_frame.bind("<Configure>",lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
-        self.canvas.pack(side="left",fill="both",expand=True)
+        self.menu_scroll = ScrollableFrame(content_frame, bg="brown")
+        self.menu_scroll.pack(fill="both", expand=True)
         
 
         intro = tk.Label(header_frame,text = "Menu Items",font= ("Arial", 43),bg="red", foreground="#fbf025",anchor="center") # Intro text
@@ -141,9 +130,14 @@ class CustomerFrame(tk.Frame): # Customer frame initialization
         sort_btn = tk.Menubutton(toolbar_frame,text = "Sort By ▼", relief="raised") # Adding the sort button menu
         sort_btn.grid(row=0,column=3, padx=5) # Set position
         sort_dropdown = tk.Menu(sort_btn,tearoff=0)
-        sort_dropdown.add_command(label = "By Price",command = self.filterprice) # Adding sorting button
+        sort_dropdown.add_command(label = "By Price ASC",command = self.filterprice_asc) # Adding sorting button
         sort_dropdown.add_separator()
-        sort_dropdown.add_command(label = "By Name",command = self.filtername)
+        sort_dropdown.add_command(label = "By Price DESC",command = self.filterprice_desc) # Adding sorting button
+        sort_dropdown.add_separator()
+        sort_dropdown.add_command(label = "By Name ASC",command = self.filtername_asc)
+        sort_dropdown.add_separator()
+        sort_dropdown.add_command(label = "By Name DESC",command = self.filtername_desc)
+        sort_dropdown.add_separator()
         sort_btn["menu"] = sort_dropdown # Link button to drop down
 
         options_btn = tk.Menubutton(header_frame,text = "▼ OPTIONS", relief="raised",bg="red") # Creating a menu drop down for options
@@ -181,16 +175,13 @@ class CustomerFrame(tk.Frame): # Customer frame initialization
         self.filter_action.grid(row=4,column = 0)
 
         self.refresh_menu()
-        self.canvas.bind("<Configure>", self.resize_canvas)
-
-    def resize_canvas(self,event):
-        self.canvas.itemconfig(self.canvas_window, width=event.width)
 
     def filterbutton(self):
         self.filter_frame.place(x=620,y =30)
 
     def filter_action(self):
         self.filter_frame.place_forget() # Close popup
+        self.category.clear() # Reset all the checkboxes list
         if self.meals_var.get() == True:
             self.category.append(1) # Append to categries if the category is ticked
         if self.snack_var.get() == True:
@@ -200,7 +191,6 @@ class CustomerFrame(tk.Frame): # Customer frame initialization
         if self.desserts_var.get() == True:
             self.category.append(4) # Append to categries if the category is ticked
         self.refresh_menu() # Refresh menu items
-        self.category.clear() # Reset all the checkboxes list
 
     def settings(self):
         self.app.show_frame(self.app.frames["settings"])
@@ -226,9 +216,8 @@ class CustomerFrame(tk.Frame): # Customer frame initialization
             price = item[5]
             col = index % 3
             rows = index // 3
-            card = ItemCard(self.item_frame,id,name,desc,img,price)
+            card = ItemCard(self.menu_scroll.content_frame,self.cartframe,id,name,desc,img,price)
             card.grid(row=rows,column=col,padx=30,pady=10)
-            #print(col,rows)
             index = index + 1
             self.cards.append(card)
         
@@ -244,7 +233,17 @@ class CustomerFrame(tk.Frame): # Customer frame initialization
         self.cards.clear()
         #self.current_items.clear()
 
-    def filterprice(self):
+    def filterprice_desc(self):
+        self.clear_menu()
+        for item in range(len(self.current_items)-1):
+            for i in range(len(self.current_items)-1):
+                if self.current_items[i][5] < self.current_items[i + 1][5]:
+                    temp = self.current_items[i]
+                    self.current_items[i] = self.current_items[i + 1]
+                    self.current_items[i + 1]= temp
+        self.display_items(self.current_items)
+
+    def filterprice_asc(self):
         self.clear_menu()
         for item in range(len(self.current_items)-1):
             for i in range(len(self.current_items)-1):
@@ -254,22 +253,76 @@ class CustomerFrame(tk.Frame): # Customer frame initialization
                     self.current_items[i + 1]= temp
         self.display_items(self.current_items)
 
+    def filtername_asc(self):
+        self.clear_menu()
+        for item in range(len(self.current_items)-1):
+            for i in range(len(self.current_items)-1):
+                if self.current_items[i][1] > self.current_items[i + 1][1]:
+                    temp = self.current_items[i]
+                    self.current_items[i] = self.current_items[i + 1]
+                    self.current_items[i + 1]= temp
+        self.display_items(self.current_items)
 
-    def filtername(self):
-        pass
-    #print("Customer Frame Initialized")
+    def filtername_desc(self):
+        self.clear_menu()
+        for item in range(len(self.current_items)-1):
+            for i in range(len(self.current_items)-1):
+                if self.current_items[i][1] < self.current_items[i + 1][1]:
+                    temp = self.current_items[i]
+                    self.current_items[i] = self.current_items[i + 1]
+                    self.current_items[i + 1]= temp
+        self.display_items(self.current_items)
 
 class CartFrame(tk.Frame): # Cart frame initialization
     def __init__(self,parent):
         super().__init__(parent) # Inherits the CustomerFrame window as the place to be displayed
+        self.cart_items = []
+        self.cart_item_cards = []
         self.configure(background="brown",width=400,height=400)
         self.pack_propagate(False) # Keep size fixed
+        content_frame = tk.Frame(self, bg="yellow") # Creating content frame for items
+        content_frame.pack(fill="both", expand=True)
+        self.cart_scroll = ScrollableFrame(content_frame, bg="brown")
+        self.cart_scroll.pack(fill="both", expand=True)
+        
+        close_btn = tk.Button(self.cart_scroll.content_frame,text = "Close", command=self.close)
+        close_btn.grid(row = 0,column = 0)
 
-        close_btn = tk.Button(self,text = "Close", command=self.close)
-        close_btn.place(x=180,y=370)
+        checkout_btn = tk.Button(self.cart_scroll.content_frame,text = "Checkout", command=self.checkout)
+        checkout_btn.grid(row = 0,column = 1)
+
+    def checkout(self):
+        self.close()
 
     def close(self):
         self.place_forget()
+
+    def display_cart(self):
+        index = 2
+        for item in self.cart_items:
+            id = item[0]
+            name = item[1]
+            price = item[2]
+            qty = item[3]
+            col = index % 2
+            rows = index // 2
+            cart_card =Cart_item_card(self.cart_scroll.content_frame,self,id,name,price,qty)
+            self.cart_item_cards.append(cart_card)
+            cart_card.grid(row = rows,column = col, padx= 5,pady=5)
+            index += 1
+
+    def clear_cart(self):
+        for card in self.cart_item_cards: # Loop through and destroy all the card objects
+            card.destroy()
+        self.cart_item_cards.clear() # Clear the cart list of card objects
+
+    def remove_item(self,item_id):
+        for item in self.cart_items: # Find the item by id
+            if item[0] == item_id:
+                self.cart_items.remove(item) # remove from list
+        self.clear_cart() # Destroy all widgets on screen
+        self.display_cart() # Redrwaw screen.
+
 
 class SettingsFrame(tk.Frame): # Settings frame initialization
     def __init__(self,app):
@@ -336,12 +389,34 @@ class OrderHistoryFrame(tk.Frame): # Order history frame initialization
     def back_menu(self):
         self.app.show_frame(self.app.frames["customerframe"])
 
+
+class ScrollableFrame(tk.Frame): # Initialize class
+    def __init__(self,parent,bg = "white"):
+        super().__init__(parent)
+        canvas_frame = tk.Frame(self, bg="brown") # Set the frame for the canvas
+        canvas_frame.pack(fill="both", expand=True) # Fill the screen as an overlay
+
+        scrollbar = tk.Scrollbar(canvas_frame,orient="vertical") # Initalize the scrollbar
+        scrollbar.pack(side="right",fill="y") # Position it
+
+        self.canvas = tk.Canvas(canvas_frame,yscrollcommand=scrollbar.set,bg=bg) # Connect the scroll bar and the canvas
+        scrollbar.config(command=self.canvas.yview) # Configure the scroll bar
+        self.content_frame = tk.Frame(self.canvas,bg =bg) # Create a frame on the canvas to write on
+        self.canvas_window = self.canvas.create_window((0,0),window=self.content_frame,anchor="nw") # Connect the canvas and the frame to use scroll bar
+        self.content_frame.bind("<Configure>",lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all"))) # Connect the canvas and the frame to use scroll bar
+        self.canvas.pack(side="left",fill="both",expand=True)
+
+        self.canvas.bind("<Configure>", self.resize_canvas) # Setting canvas size
+
+    def resize_canvas(self,event):
+        self.canvas.itemconfig(self.canvas_window, width=event.width) # Configure canvas tro match frame size
+
 class ItemCard(tk.Frame): # Initializes the class
-    def __init__(self, parent, item_id, name, description, image, price): # All the parameters
+    def __init__(self, parent,cartframe, item_id, name, description, image, price): # All the parameters
         super().__init__(parent) # Inherits from the customer frame class
         self.configure(background="orange",width =200,height=230)
         self.grid_propagate(False)
-        
+        self.cartframe = cartframe
         self.item_id = item_id
         self.name = name
         self.description = description
@@ -350,6 +425,8 @@ class ItemCard(tk.Frame): # Initializes the class
         self.popup = None
         self.image = self.image_resize(self.old_image)
         self.quantity = 0
+
+        
 
         self.clickable_widgets(self)
         
@@ -384,9 +461,10 @@ class ItemCard(tk.Frame): # Initializes the class
         tk.Label(self.popup, image=self.image).pack(pady=5)
         tk.Label(self.popup, text=f"This item is a {self.description}",wraplength=150).pack(pady=10)
         tk.Label(self.popup, text=f"One portion costs: £{self.price:.2f}").pack(pady=10)
-        self.qty = tk.Spinbox(self.popup, from_=0,to=10,width=10,command = self.quantity_value,state="normal",justify="center",wrap="True")
+        self.qty = tk.Spinbox(self.popup, from_=0,to=10,width=10,state="normal",justify="center",wrap="True")
+        self.qty.config(state="readonly")
         self.qty.pack()
-        tk.Button(self.popup,text="Add to chart").pack(pady=5)
+        tk.Button(self.popup,text="Add to chart",command=self.add_to_cart).pack(pady=5)
         tk.Button(self.popup,text="Close",command=self.close_popup).pack(pady=5)
         self.popup.protocol("WM_DELETE_WINDOW", self.close_popup)
 
@@ -399,8 +477,61 @@ class ItemCard(tk.Frame): # Initializes the class
         img = ImageTk.PhotoImage(img)
         return img
 
-    def quantity_value(self):
-        self.quantity = int(self.qty.get())
-        #print(f"Quantity is now {self.quantity}")
+
+    def add_to_cart(self):
+        if int(self.qty.get()) > 0 : # Check if the quantity is above 0 and under 10
+            cart_item = [self.item_id,self.name,self.price,int(self.qty.get())] # Get the item id name price and quantity
+            for item in self.cartframe.cart_items: # Check if there is an instance of that specific item
+                if item[0] == self.item_id:
+                    if item[3] >= 10 or (item[3] + int(self.qty.get())) > 10: # Check if max order limit per item is reached
+                        messagebox.showerror("Invalid Order", "Order quantity exceeds limit of 10") # Show error message
+                        self.close_popup()
+                        return
+                    else:
+                        item[3] += int(self.qty.get()) # If max limit is not reached calc quantity
+                        self.cartframe.display_cart() # Update display
+                        self.close_popup()
+                        return
+            self.cartframe.cart_items.append(cart_item) # Add new item to cart
+            self.cartframe.display_cart() # Update display
+            self.close_popup()
+        else: 
+            messagebox.showerror("Invalid Order", "Order quantity is below 1") # Display error message.
+            self.close_popup()
+
+
+class Cart_item_card(tk.Frame):
+    def __init__(self, parent,cartframe, item_id, name, price,quantity):
+        super().__init__(parent)
+        self.configure(background="orange",width =180,height=100)
+        self.grid_propagate(False)
+        self.item_id = item_id
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.popup = None
+        self.cartframe = cartframe
         
+
+        
+        name = tk.Label(self,text = self.name, anchor="center")
+        name.grid(row=0,column=0,columnspan=2,padx=13,pady=5)
+
+
+        value = self.price * self.quantity
+        price = tk.Label(self, text=f"Price: £{value:.2f}", anchor="center")
+        price.grid(row=2,column=0,columnspan=2,padx=13,pady=5)
+
+
+        quantity = tk.Label(self, text=f"Quantity: {self.quantity}", anchor="center")
+        quantity.grid(row=1,column=0,columnspan=2,padx=13,pady=5)
+
+        remove_btn = tk.Button(self,text = "Remove",command=self.remove_item)
+        remove_btn.grid(row=1,column=3)
+
+    def remove_item(self):
+        self.cartframe.remove_item(self.item_id)
+       
+    
+    
 MainApp()
